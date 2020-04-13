bddb33e9482762a9857d4ae926372610218df5d9
encik-ijak <encikijak@gmail.com>
2020-04-13 22:44:53 +0800

    commit

--- LearningJDBC/src/main/java/nm/wd/jdbc/JDBCExecutor.java
+++ LearningJDBC/src/main/java/nm/wd/jdbc/JDBCExecutor.java
@@ -1,7 +1,24 @@
 package nm.wd.jdbc;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class JDBCExecutor {
     public static void main(String[] args) {
-        System.out.println("Helloo");
+        DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost",
+                "hplussport", "postgres", "P@ssword0");
+
+        try {
+            Connection connection = dcm.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM CUSTOMER");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
